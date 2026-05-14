39219842375807802f8fa3b437cbdde766e85627
Mor4n <anm0ran@outlook.com>
2026-05-14 08:38:44 -0700

    feat(ui): add Header Component and fonts in Index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Header from "./components/Header"
 
 
 function App() {
 
   return (
     <>
-   
+      <Header/>
+
+      
+
+
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header =() => {
+
+    return (
+        <header class="top-banner" id="encabezado">
+            <p>Envios gratis a partir de $500.00</p>
+        </header>
+    )
+
+}
+
+export default Header
+
