5778326090d660919ab14484af9c8574fbfc5d81
Mor4n <anm0ran@outlook.com>
2026-05-14 08:47:20 -0700

    feat(ui): add Nav Component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,39 @@
+
+import React from 'react'
+
+function Nav() {
+  return (
+    <>
+        <nav id="navegacion">
+        <h1 id="logo">
+            <a href="#encabezado"><img src="./img/logo.png" alt="Logo de Hoshi-Store" id="logo-img"/></a>
+        </h1>
+
+
+        <ul class="navegacion-lista">
+            <li><a href="#catalogo">Catalogo</a></li>
+            <li><a href="#compra">Proceso de compra</a></li>
+            <li><a href="#sobre">Sobre nosotros</a></li>
+            <li><a href="#fotos">Fotos de la comunidad</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+        </ul>
+
+        <div class="buscar-contenedor">
+            <input type="text" placeholder="Ingrese aquí el producto a buscar" id="buscar-input"/>
+            <button type="button" id="buscar-boton">
+                <img src="./img/lupa.png" alt="Ícono de lupa" id="buscar-imagen"/>
+            </button>
+        </div>
+
+        <div class="carrito-contenedor">
+            <button type="button" id="carrito-boton">
+                <img src="./img/carrito.png" alt="Imagen de carrito de compras" id="carrito-compras"/>
+            </button>
+        </div>
+
+    </nav>
+    </>
+  )
+}
+
+export default Nav
