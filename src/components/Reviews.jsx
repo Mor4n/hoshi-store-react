2b25585418a7eb1f1169d68ec9a1cf2d9b82635e
Mor4n <anm0ran@outlook.com>
2026-05-14 13:39:15 -0700

    fix(ui): fix className typos

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 function Reviews() {
   return (
-    <section id="fotos" class="fotos-section">
+    <section id="fotos" className="fotos-section">
             <h2>Fotos de la comunidad</h2>
             <p>Esta es una galería de fotos de los productos que nuestra comunidad ha adquirido, tu también puedes ser parte de esto, añade tu foto en el formulario del final y te añadiremos 💕</p>
 
-            <div class="fotos-contenedor">
-                    <div class="foto f1"></div>
-                    <div class="foto f2"></div>
-                    <div class="foto f3"></div>
-                    <div class="foto f4"></div>
-                    <div class="foto f5"></div>
-                    <div class="foto f6"></div>
-                    <div class="foto f7"></div>
-                    <div class="foto f8"></div>
-                    <div class="foto f9"></div>
-                    <div class="foto f10"></div>
+            <div className="fotos-contenedor">
+                    <div className="foto f1"></div>
+                    <div className="foto f2"></div>
+                    <div className="foto f3"></div>
+                    <div className="foto f4"></div>
+                    <div className="foto f5"></div>
+                    <div className="foto f6"></div>
+                    <div className="foto f7"></div>
+                    <div className="foto f8"></div>
+                    <div className="foto f9"></div>
+                    <div className="foto f10"></div>
             </div>
 
     </section>
