9e1f16bae52b51fa42996a2d1cfbf84d665952ce
Mor4n <anm0ran@outlook.com>
2026-05-14 13:53:24 -0700

    feat(cart): update product quantity if addCart is pressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,67 @@
-import { useState } from "react"
-import { flecha, tienda_online } from "./barrels/assets"
-import Header from "./components/Header"
-import Nav from "./components/Nav"
-import ProductCatalog from "./components/ProductCatalog"
-import db from "./data/db"
-import PurchaseProcess from "./components/PurchaseProcess"
-import About from "./components/About"
-import TeamCard from "./components/common/TeamCard"
-import Reviews from "./components/Reviews"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
-
+import { useState } from "react";
+import { flecha, tienda_online } from "./barrels/assets";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import ProductCatalog from "./components/ProductCatalog";
+import db from "./data/db";
+import PurchaseProcess from "./components/PurchaseProcess";
+import About from "./components/About";
+import TeamCard from "./components/common/TeamCard";
+import Reviews from "./components/Reviews";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-
   const [data] = useState(db);
 
-  const [cart,useCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
-  const addToCart = (producto) =>{
+  const addToCart = (producto) => {
+    // Revisar si ya existe el producto en el carrito
+    const findProductIndex = cart.findIndex((item) => item.id === producto.id);
 
-    // Le añado la propiedad cantidad en 1
-    producto.cantidad = 1;
+    if (findProductIndex < 0) {
+      console.log("No existe, añadiendo");
 
-    // Se lo establezco como nuevo valor en carrito
-    useCart( (prevCart)  => [...prevCart,producto] )
+      // Le añado la propiedad cantidad en 1
+      producto.cantidad = 1;
 
-  }
+      // Se lo establezco como nuevo valor en carrito
+      setCart((prevCart) => [...prevCart, producto]);
+    } else {
+      
+      console.log("Ya existe, añadiendo cantidad");
+
+      const carritoActualizado = [...cart];
+
+      carritoActualizado[findProductIndex].cantidad +=1
+
+      setCart(carritoActualizado);
+      
+    }
+  };
 
   return (
     <>
-      <Header/>
+      <Header />
 
-      <Nav  cart={cart}  />
+      <Nav cart={cart} />
 
       <main>
         <div id="video-contenedor">
           <div id="video-grisaceo">
             <p id="video-texto">Somos Hoshi-Store</p>
           </div>
-          <video src={tienda_online} id="video-tienda" autoPlay muted loop></video>
+          <video
+            src={tienda_online}
+            id="video-tienda"
+            autoPlay
+            muted
+            loop
+          ></video>
         </div>
 
-        <ProductCatalog data={data}  addToCart={addToCart}/>
+        <ProductCatalog data={data} addToCart={addToCart} />
 
         <PurchaseProcess />
 
@@ -51,19 +70,25 @@ function App() {
         <Reviews />
 
         <Contact />
-
       </main>
 
       <Footer />
 
-      <a href="#" className="boton-whatsapp"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/960px-WhatsApp.svg.png" alt="Imagen de logo de Whatsapp" /></a>
-      <a href="#encabezado" className="boton-subir">
-        <img src={flecha} alt="Botón de subir hacia arriba" id="img-boton-subir" />
+      <a href="#" className="boton-whatsapp">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/960px-WhatsApp.svg.png"
+          alt="Imagen de logo de Whatsapp"
+        />
       </a>
-
-
+      <a href="#encabezado" className="boton-subir">
+        <img
+          src={flecha}
+          alt="Botón de subir hacia arriba"
+          id="img-boton-subir"
+        />
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
